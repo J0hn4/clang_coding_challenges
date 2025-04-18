591a4e0c6fe79e72f410607d1037097283e30325
The Professor <prof_x22@protonmail.com>
2025-04-17 21:24:27 -0400

    reverse array

---
+++ reverse_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+
+    // Logic to reverse the array in-place
+    int temp;
+    for(i = 0; i < num / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[num - 1 - i];
+        arr[num - 1 - i] = temp;
+    }
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+
+    free(arr); // Don't forget to free the allocated memory
+    return 0;
+}
